f3590e2ebca039fb053b984cf342f14e5e7c13d2
Dan Vanderkam <danvdk@gmail.com>
2024-11-29 16:25:14 -0500

    day 3 star 2

--- lib/day3.exs
+++ lib/day3.exs
@@ -10,6 +10,12 @@ defmodule Day3 do
   def is_valid([a, b, c]) do
     a + b > c and a + c > b and b + c > a
   end
+
+  def transpose(ary) do
+    for i <- 0..2, do:
+      (for j <- 0..2, do:
+        Enum.at(Enum.at(ary, j), i))
+  end
 end
 
 input_file = hd(System.argv())
@@ -20,3 +26,17 @@ instrs = Common.read_lines(input_file)
 part1valid = instrs |> Enum.filter(&Day3.is_valid/1)
 # IO.inspect(instrs)
 IO.puts(part1valid |> Enum.count())
+
+# IO.inspect(instrs)
+# first3 = instrs |> Enum.take(3)
+# IO.inspect(first3)
+# first3tr = first3 |> Day3.transpose()
+# IO.inspect(first3tr)
+
+instrs2 = Enum.chunk_every(instrs, 3)
+  |> Enum.map(&Day3.transpose/1)
+  |> Enum.flat_map(&(&1))
+part2valid = instrs2 |> Enum.filter(&Day3.is_valid/1)
+IO.puts(part2valid |> Enum.count())
+
+# IO.inspect(instrs2)
