66dbcbe1415ea4d2cada2dcc25c971302ab43f8d
Dan Vanderkam <danvdk@gmail.com>
2024-12-04 07:51:18 -0500

    diagonals allowed! day 4 star 1

--- lib/day4.ex
+++ lib/day4.ex
@@ -4,13 +4,39 @@ defmodule Day4 do
     String.split(line)
   end
 
+  def chars_in_dir(grid, {x, y}, {dx, dy}) do
+    Enum.map(0..3, fn i -> grid[{x + i * dx, y + i * dy}] end)
+  end
+
   def main(input_file) do
     grid =
-      for {i, line} <- Util.read_lines(input_file) |> Util.enumerate(),
-          {j, char} <- line |> String.to_charlist() |> Util.enumerate(),
+      for {y, line} <- Util.read_lines(input_file) |> Util.enumerate(),
+          {x, char} <- line |> String.to_charlist() |> Util.enumerate(),
           into: %{},
-          do: {{i, j}, char}
+          do: {{x, y}, char}
 
-    IO.inspect(grid)
+    # IO.inspect(grid)
+
+    dirs = [
+      {1, 0},
+      {-1, 0},
+      {0, 1},
+      {0, -1},
+      {1, 1},
+      {-1, -1},
+      {1, -1},
+      {-1, 1}
+    ]
+
+    w = for({x, _y} <- Map.keys(grid), do: x) |> Enum.max()
+    h = for({_x, y} <- Map.keys(grid), do: y) |> Enum.max()
+    IO.inspect({w, h})
+
+    part1 =
+      for(x <- 0..w, y <- 0..h, dir <- dirs, do: {{x, y}, dir, chars_in_dir(grid, {x, y}, dir)})
+      |> Enum.filter(fn {_, _, chars} -> chars == ~c"XMAS" end)
+      |> Enum.count()
+
+    IO.inspect(part1)
   end
 end
