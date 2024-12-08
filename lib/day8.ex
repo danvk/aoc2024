f53b9a1636c1debb8a48b11eaee2c8f50c4874b0
Dan Vanderkam <danvdk@gmail.com>
2024-12-08 09:37:25 -0500

    day 8 star 2

--- lib/day8.ex
+++ lib/day8.ex
@@ -12,6 +12,14 @@ defmodule Day8 do
     |> Enum.filter(fn {x, y} -> x >= 0 && y >= 0 && x <= w && y <= h end)
   end
 
+  def antinodes2({ax, ay}, {bx, by}, {w, h}) do
+    dx = bx - ax
+    dy = by - ay
+    as = for i <- 0..max(w, h), do: {ax - i * dx, ay - i * dy}
+    bs = for i <- 0..max(w, h), do: {bx + i * dx, by + i * dy}
+    (as ++ bs) |> Enum.filter(fn {x, y} -> x >= 0 && y >= 0 && x <= w && y <= h end)
+  end
+
   def main(input_file) do
     {grid, wh} = Util.read_grid(input_file)
     grid = Map.filter(grid, fn {_k, v} -> v != ?. end)
@@ -30,6 +38,15 @@ defmodule Day8 do
 
     # Util.inspect(part1)
     Util.inspect(part1 |> Enum.count())
+
+    part2 =
+      antennae
+      |> Enum.flat_map(fn {_freq, spots} ->
+        pairs(spots) |> Enum.flat_map(fn {a, b} -> antinodes2(a, b, wh) end)
+      end)
+      |> Enum.uniq()
+
+    Util.inspect(part2 |> Enum.count())
 
     # Util.inspect(antinodes({4, 4}, {5, 2}, wh))
     # Util.inspect(pairs(antennae[?0]))
