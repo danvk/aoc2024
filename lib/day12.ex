2a0161f0c7f8e274f5da0f167603e1989b530f07
Dan Vanderkam <danvdk@gmail.com>
2024-12-12 09:05:23 -0500

    relabeling; still not right

--- lib/day12.ex
+++ lib/day12.ex
@@ -13,15 +13,47 @@ defmodule Day12 do
     Enum.filter(@dirs, fn {dx, dy} -> Map.get(grid, {x + dx, y + dy}) != c end) |> Enum.count()
   end
 
+  def relabel(grid) do
+    grid
+    |> Enum.reduce({?A, %{}}, fn {pt, v}, {n, new_grid} ->
+      {new_grid, v, next_n} =
+        cond do
+          Map.get(new_grid, pt) != nil -> {new_grid, v, n}
+          true -> {Map.put(new_grid, pt, n), n, n + 1}
+        end
+
+      c = Map.get(grid, pt)
+      {x, y} = pt
+
+      new_grid =
+        @dirs
+        |> Enum.reduce(new_grid, fn {dx, dy}, new_grid ->
+          npt = {x + dx, y + dy}
+
+          if Map.get(grid, npt) == c do
+            Map.put(new_grid, npt, v)
+          else
+            new_grid
+          end
+        end)
+
+      {next_n, new_grid}
+    end)
+    |> Util.second()
+  end
+
   def main(input_file) do
-    {grid, wh} = Util.read_grid(input_file)
-    Util.print_grid(grid, wh)
+    {grid, _wh} = Util.read_grid(input_file)
+    # Util.print_grid(grid, wh)
+
+    grid = relabel(grid)
+    # Util.print_grid(grid, wh)
 
     by_crop =
       Enum.group_by(Map.to_list(grid), &Util.second(&1))
       |> Util.map_values(fn _k, v -> v |> Enum.map(&Util.first/1) end)
 
-    Util.inspect(by_crop)
+    # Util.inspect(by_crop)
 
     areas = Util.map_values(by_crop, fn _k, v -> Enum.count(v) end)
     Util.inspect(areas)
